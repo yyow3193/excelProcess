d5e139152df1ae1930d67aa9f01014c7903c6628
yyow3193 <420321897@163.com>
2021-03-05 19:16:41 +0800

    会计账本合并

--- main.py
+++ main.py
@@ -31,43 +31,39 @@ month = file_name[monthindex]
 book_everymonth_everypersion = xlwt.Workbook(encoding='utf-8')
 
 name2newsheet = {}
-name2totalrow = {}
+name2recordsumlist = {}
+
+name2recordlist = {}
 titlerow = None
 
 # 汇总每个月的每个人
-allsheet = book_everymonth_everypersion.add_sheet("all", cell_overwrite_ok=True)
-
-
 for sheet in rdata.sheets():
     print("open sheet index:", sheet.name)
     for rowindex in range(sheet.nrows):
-        if rowindex <= 2:
+        if rowindex <= 2: # 前两行是公司名
             if titlerow == None and rowindex == 2:
                 titlerow = sheet.row(rowindex)
             continue
         row = sheet.row(rowindex)
         if row[5].value == "":
             continue
-        newsheet = None
-        if row[5].value in name2newsheet:
-            newsheet = name2newsheet[row[5].value]
-            name2totalrow[row[5].value][2].value = name2totalrow[row[5].value][2].value + row[2].value
-            name2totalrow[row[5].value][3].value = name2totalrow[row[5].value][3].value + row[3].value
-            name2totalrow[row[5].value][4].value = name2totalrow[row[5].value][4].value + row[4].value
+
+        if row[5].value in name2recordlist:
+            recordlist = name2recordlist[row[5].value]
+            recordlist.append(row)
+
+            name2recordsumlist[row[5].value][2].value = name2recordsumlist[row[5].value][2].value + row[2].value
+            name2recordsumlist[row[5].value][3].value = name2recordsumlist[row[5].value][3].value + row[3].value
+            name2recordsumlist[row[5].value][4].value = name2recordsumlist[row[5].value][4].value + row[4].value
 
         else:
-            newsheet = book_everymonth_everypersion.add_sheet(row[5].value, cell_overwrite_ok=True)
-            name2newsheet[row[5].value] = newsheet
+            recordlist = []
+            name2recordlist[row[5].value] = recordlist
+            titlerow = sheet.row(2) # 这一行是列名
+            name2recordsumlist[row[5].value] = copy.deepcopy(row)
+            recordlist.append(titlerow)
+            recordlist.append(row)
 
-            titlerow = sheet.row(2)
-            for colindex in range(len(titlerow)):
-                newsheet.write(0, colindex, titlerow[colindex].value)
-
-            name2totalrow[row[5].value] = copy.deepcopy(row)
-
-        if(len(allsheet.get_rows()) <= 0):
-            for colindex in range(len(titlerow)):
-                allsheet.write(0, colindex, titlerow[colindex].value)
 
         # style = xlwt.XFStyle()  # 初始化样式
         # font = xlwt.Font()  # 为样式创建字体
@@ -106,49 +102,39 @@ for sheet in rdata.sheets():
         # # 定义格式
         # style.borders = borders
 
+def comp(row):
+    if isinstance(row[4].value, str) :
+        return 1999999999
+    return row[4].value
 
+for (k, v) in name2recordlist.items():
+    persionname = k
+    recordlist = v
+    recordlist.sort(reverse=True, key=comp)
 
-        nowrowcount = len(newsheet.get_rows())
+# 月度汇总表
+recordlist = []
+sumsheet = book_everymonth_everypersion.add_sheet("all", cell_overwrite_ok=True)
+for (k, v) in name2recordsumlist.items():
+    recordlist.append(v)
+
+recordlist.sort(reverse=True, key=comp)
+for rowi in range(len(recordlist)):
+    row = recordlist[rowi]
+    for colindex in range(len(row)):
+        sumsheet.write(rowi, colindex, row[colindex].value)
+
+    persionname = row[5].value
+    everypersionRecordlist = name2recordlist[persionname]
+    newsheet = book_everymonth_everypersion.add_sheet(persionname, cell_overwrite_ok=True)
+    for rowi in range(len(everypersionRecordlist)):
+        row = everypersionRecordlist[rowi]
         for colindex in range(len(row)):
-            newsheet.write(nowrowcount, colindex, row[colindex].value)
-            #print(row[colindex].value)
-
-for (k, v) in name2totalrow.items():
-    nowrowcount = len(allsheet.get_rows())
-    print(v)
-    for colindex in range(len(v)):
-        allsheet.write(nowrowcount, colindex, v[colindex].value)
+            newsheet.write(rowi, colindex, row[colindex].value)
 
 
 everymonth_everypersion_book_name = "book_everymonth_everypersion_" + month + ".xls"
 book_everymonth_everypersion.save(everymonth_everypersion_book_name)
-
-
-# everymonth_everypersion_book_name=os.getcwd()+'\\'+everymonth_everypersion_book_name
-# rdata1=xlrd.open_workbook(everymonth_everypersion_book_name)
-#
-# allsheet = rdata1.sheet_by_name("all")
-# print(allsheet.name)
-#
-# for sheet in rdata.sheets():
-#     allrow = None
-#     for rowindex in range(sheet.nrows):
-#         if(rowindex==0):
-#             continue
-#
-#         for rowindex in range(sheet.nrows):
-#             row = sheet.row(rowindex)
-#             if (allrow == None):
-#                 allrow = copy.deepcopy(row)
-#             else:
-#                 allrow[2].value = allrow[2].value + row[2].value
-#                 allrow[3].value = allrow[3].value + row[3].value
-#                 allrow[4].value = allrow[4].value + row[4].value
-#                 allrow[9].value = allrow[9].value + row[9].value
-
-
-
-
 
 
 
