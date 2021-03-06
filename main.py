b91c5ec352a54189167600b32f46f65604dfc267
fangxing2 <420321897@163.com>
2021-03-06 23:44:44 +0800

    重构了一下，加了些错误提示

--- main.py
+++ main.py
@@ -5,6 +5,19 @@ import xlrd
 import xlwt
 import copy
 
+name_colindex = 5  # “收款单位”  所在的列 index
+colname_rowindex = 2  # 列名所在的index
+weight_colindex = 2  # "重量" 所在的列index
+hanshuie_colindex = 3  # "含税额" 列index
+fukuanjiane_colindex = 4  # "付款金额" 列index
+
+summary_colindex_list = [2, 3, 4]
+
+def getColChar(colindex):
+    return chr(colindex + 65) + "列"
+
+def getRowNumber(rowindex):
+    return str(rowindex + 1) + "行"
 
 class MonthBook:
 
@@ -30,21 +43,28 @@ class MonthBook:
         for sheet in self.rdata.sheets():  # 每个月内的每一天是一个sheet
             # print("open sheet name:", sheet.name)
             for rowindex in range(sheet.nrows):
-                if rowindex <= 2:  # 前两行是公司名
-                    if self.titlerow == None and rowindex == 2:
+                if rowindex <= colname_rowindex:  # 前两行是公司名
+                    if self.titlerow == None and rowindex == colname_rowindex:
                         self.titlerow = sheet.row(rowindex)
                     continue
                 row = sheet.row(rowindex)
-                if row[5].value == "":
+                if row[name_colindex].value == "":
                     continue
+                # 以下代码用来检查该字段能不能汇总, 仅仅用于错误检查
+                for colindex in summary_colindex_list:
+                    try:
+                        testfieldtype = row[colindex].value + 1
+                    except TypeError:
+                        print("表 " + self.excelname + " sheet:" + sheet.name +" 单元格:" + getColChar(colindex) + getRowNumber(
+                            rowindex) + "不支持加法，请检查是否有数字,", TypeError)
 
-                if row[5].value in self.name2recordlist:
-                    recordlist = self.name2recordlist[row[5].value]
+                if row[name_colindex].value in self.name2recordlist:
+                    recordlist = self.name2recordlist[row[name_colindex].value]
                     recordlist.append(row)
                 else:
                     recordlist = []
-                    self.name2recordlist[row[5].value] = recordlist
-                    self.titlerow = sheet.row(2)  # 这一行是列名
+                    self.name2recordlist[row[name_colindex].value] = recordlist
+                    self.titlerow = sheet.row(colname_rowindex)  # 这一行是列名
                     recordlist.append(self.titlerow)
                     recordlist.append(row)
 
@@ -66,9 +86,13 @@ class MonthBook:
                 if i == 1:
                     continue  # 列名不要加进去了
                 if k in self.name2recordsum:
-                    self.name2recordsum[k][2].value = self.name2recordsum[k][2].value + record[2].value
-                    self.name2recordsum[k][3].value = self.name2recordsum[k][3].value + record[3].value
-                    self.name2recordsum[k][4].value = self.name2recordsum[k][4].value + record[4].value
+                        self.name2recordsum[k][weight_colindex].value = self.name2recordsum[k][weight_colindex].value + \
+                                                                        record[weight_colindex].value
+                        self.name2recordsum[k][hanshuie_colindex].value = self.name2recordsum[k][hanshuie_colindex].value + \
+                                                                          record[hanshuie_colindex].value
+                        self.name2recordsum[k][fukuanjiane_colindex].value = self.name2recordsum[k][
+                                                                                 fukuanjiane_colindex].value + record[
+                                                                                 fukuanjiane_colindex].value
                 else:
                     recordcopy = copy.deepcopy(record)
                     self.name2recordsum[k] = recordcopy
@@ -81,11 +105,11 @@ class MonthBook:
             if row != self.titlerow:
                 row[0].value = rowi
             for colindex in range(len(row)):
-                if colindex <= 5:
+                if colindex <= name_colindex:
                     sumsheet.write(rowi, colindex, row[colindex].value)
             if row == self.titlerow:
                 continue
-            persionname = row[5].value
+            persionname = row[name_colindex].value
             everypersionRecordlist = self.name2recordlist[persionname]
             newsheet = book_month_summary.add_sheet(persionname, cell_overwrite_ok=True)
             for rowi in range(len(everypersionRecordlist)):
@@ -93,18 +117,18 @@ class MonthBook:
                 if row != self.titlerow:
                     row[0].value = rowi
                 for colindex in range(len(row)):
-                    if colindex <= 5:
+                    if colindex <= name_colindex:
                         newsheet.write(rowi, colindex, row[colindex].value)
 
         month_summary_name = "./output/book_month_summary_" + self.month + ".xls"
         book_month_summary.save(month_summary_name)
-        print("success 生成：", month_summary_name)
+        print("     生成success：", month_summary_name)
 
 
 def comp(row):
-    if isinstance(row[4].value, str):
+    if isinstance(row[fukuanjiane_colindex].value, str):
         return sys.maxsize
-    return row[4].value
+    return row[fukuanjiane_colindex].value
 
 
 class YearStatistics:
@@ -131,9 +155,12 @@ class YearStatistics:
                 titleRow = book.titlerow
             for (name, summary) in monthsummarys.items():
                 if name in name2summary:
-                    name2summary[name][2].value = name2summary[name][2].value + summary[2].value
-                    name2summary[name][3].value = name2summary[name][3].value + summary[3].value
-                    name2summary[name][4].value = name2summary[name][4].value + summary[4].value
+                    name2summary[name][weight_colindex].value = name2summary[name][weight_colindex].value + summary[
+                        weight_colindex].value
+                    name2summary[name][hanshuie_colindex].value = name2summary[name][hanshuie_colindex].value + summary[
+                        hanshuie_colindex].value
+                    name2summary[name][fukuanjiane_colindex].value = name2summary[name][fukuanjiane_colindex].value + \
+                                                                     summary[fukuanjiane_colindex].value
                 else:
                     summaryCopy = copy.deepcopy(summary)
                     name2summary[name] = summaryCopy
@@ -146,12 +173,12 @@ class YearStatistics:
             if row != titleRow:
                 row[0].value = rowi
             for colindex in range(len(row)):
-                if colindex <= 5:
+                if colindex <= name_colindex:
                     sumsheet.write(rowi, colindex, row[colindex].value)
 
         year_summary_name = "./output/book_year_summary" + ".xls"
         book_year_summary.save(year_summary_name)
-        print("success 生成：", year_summary_name)
+        print("     生成success：", year_summary_name)
 
 
 def main():
@@ -171,7 +198,9 @@ def main():
     os.system('pause')
 
 
-
-
 if __name__ == '__main__':
     main()
+
+
+def test():
+    pass
